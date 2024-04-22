fa315a0e032f83332f5968914fc43db8e859dba8
Racheal Orukele <orukele_racheal@yahoo.com>
2024-04-22 18:01:52 +0100

    feat: add image from local machine & display in bank-container

--- cards.js
+++ cards.js
@@ -1,4 +1,61 @@
 const cards = document.querySelectorAll(".card");
+const uploadFile = document.querySelector("#choose-files");
+
+// Upload file from computer logic
+const uploadFileToBank = (e) => {
+  console.log("try upload");
+
+  const file = createFile();
+  const filesBank = document.querySelector(".files-bank");
+  filesBank.appendChild(file);
+};
+
+uploadFile.onclick = uploadFileToBank;
+
+const createFile = () => {
+  console.log("try create");
+  const file = document.createElement("div");
+  file.classList.add("card");
+  file.setAttribute("draggable", "true");
+  file.id = Date.now();
+  file.ondragstart = onDragStart;
+  file.ondragend = onDragEnd;
+  file.onclick = deleteCard;
+  appendImage(file);
+
+  return file;
+};
+
+const appendImage = (card) => {
+  const input = document.createElement("input");
+  input.setAttribute("type", "file");
+  input.setAttribute("accept", "image/png,image/gif,image/jpeg,image/jpg");
+  input.style.visibility = "hidden";
+  input.onchange = () => {
+    const image = new Image(82, 82);
+    const firstImage = input.files[0];
+    const fileName = document.querySelector("#uploaded-image-filename");
+    fileName.innerHTML = firstImage.name;
+    console.log(firstImage, firstImage.name);
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      image.src = e.target.result;
+      image.style.pointerEvents = "none";
+      card.appendChild(image);
+    };
+    reader.readAsDataURL(firstImage);
+  };
+  input.click();
+};
+
+const deleteCard = (e) => {
+  console.log("delete");
+  const onDeleteCard = window.confirm("Do you want to delete this file?");
+  if (onDeleteCard) {
+    e.target.remove();
+  }
+};
 
 const onDragStart = (e) => {
   console.log("dragging element");
